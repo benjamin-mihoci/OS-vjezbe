cedf952e36882ec8ca4094b215e426e3242d1a16
benito <benito@debianchek.192.168.100>
2026-03-19 05:52:40 +0100

    dodan peti

---
+++ OS1-zadace/zadatak5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir vjezba5
+
+rendom_ime="$RANDOM"
+
+touch vjezba5/${rendom_ime}.num
+# postoji li datoteka mozemo provjeriti naredbom test koj
+if test -f vjezba5/${rendom_ime}.num; then
+	echo "datoteka postoji"
+	echo "pa radim kopiju datoteke: "
+	cp vjezba5/${rendom_ime}.num vjezba5/backup.num
+	mkdir vjezba5/backup
+	mv vjezba5/backup.num vjezba5/backup/backup.num
+fi
+
+
+
