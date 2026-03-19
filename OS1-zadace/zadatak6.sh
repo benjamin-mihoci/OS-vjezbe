fed8c09239b59ce8c4730803ee18ae5c862acfe7
benito <benito@debianchek.192.168.100>
2026-03-19 05:58:03 +0100

    DODAVANJE SESTOGA

---
+++ OS1-zadace/zadatak6.sh
@@ -0,0 +1,7 @@
+mkdir ~/Documents/vjezba6
+touch ~/Documents/{OS_script.md,notes.txt}
+mkdir ~/Documents/scripts
+mv ~/Documents/OS_script.md ~/Documents/scripts/
+mv ~/Documents/notes.txt ~/Documents/todo.txt
+cd ~/Documents/scripts
+rm ../todo.txt
